b26d1ab7395770a7e527a229f2816d76f8c864a7
Bymarium <bymarium@gmail.com>
2025-01-18 23:00:52 -0500

    style(right): create component for the scrollable part of main

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { LeftComponent } from './left/left.component';
+import { RightComponent } from "./right/right.component";
 
 @Component({
   selector: 'app-main',
-  imports: [LeftComponent],
+  imports: [LeftComponent, RightComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
