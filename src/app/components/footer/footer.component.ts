f7fd11b7e2e95fa60ed4428502efa089e22febb7
Bymarium <bymarium@gmail.com>
2025-01-19 01:02:12 -0500

    refactor(footer): improve footer component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  public links =[
+    'https://docs.glow.app/',
+    'https://glow.app/support',
+    'https://glow.app/terms',
+    'https://glow.app/privacy-policy',
+    'https://glow.app/bug-bounty',
+    'https://twitter.com/glowwallet'
+  ];
+  public names = ['Docs', 'Support', 'Terms', 'Privacy', 'Bug Bounty'];
 }
