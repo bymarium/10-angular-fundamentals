dc60029dee4709a5610d1fed4c048cbfb15ef1fe
Bymarium <bymarium@gmail.com>
2025-01-18 22:22:15 -0500

    style(left): create component for the static part of the main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { MainComponent } from './components/main/main.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
